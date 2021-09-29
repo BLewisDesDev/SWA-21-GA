2e1388066aa88139f347676ab3ea73078aa6c7ce
Byron <b.lewis.design.development@gmail.com>
2021-09-29 16:20:55 +1000

    Commit2Testing
    TestingVersion Controll

--- Project1.0.R
+++ Project1.0.R
@@ -1,32 +1,56 @@
 # -- -- -- SWA Group XXXX Major Project -- -- -- #
 
-# -- Import Data and Libraries
-
+# -- Libraries
 library(plyr)
 library(dplyr)
 library(readxl)
+library(mosaic) #
+library(ggplot2)
+
+# -- Directories
+
+#Omer
+#directory<-"C:"
+
+#Amir
+#directory<-"C:"
+
+#
+#directory<-"D:/R studio/Projects"
+
+#Byron
+directory<-"/Users/CollectiveX/Desktop/Repos/SWA-21-GA"
+setwd(directory)
 
 # -- -- Question 8.1 -- -- -- #
 
 # -- 8.1.1
 tweets = read.csv("tweets.csv", as.is = TRUE)
 count(tweets,source)
-
-TweetSource <- table(tweets$source)
-TweetSource
+TSTable = table(tweets$source)
+TSDF = t(as.data.frame(TSTable))
+rownames(TSDF) = c("Source Name","Frequency")
+TSDF
 
 # -- 8.1.2
 random = read.csv("random.csv", as.is = TRUE)
 count(random,source)
+RSTable = table(random$source)
+RSDF = t(as.data.frame(RSTable))
+rownames(RSDF) = c("Source Name","Frequency")
+RSDF
 
-RandSource <- table(random$source)
-RandSource
+# -- 8.1.3 (Create table)
+# Table with all unique sources
+sourceTable <- matrix(c(1:104), nrow=2, byrow=TRUE)
 
-# -- 8.1.3
-#join(TweetSource, RandSource, by=NULL, type="full", match="all")
+colnames(sourceTable) <- union(names(TSTable), names(RSTable))
+rownames(sourceTable) <- c("Tweets","Random")
+print(sourceTable)
 
-sourceTable <- matrix(c(1,2,3,4,5,6,7,8), nrow=2, byrow=TRUE)
-colnames(sourceTable) <- c("iPhone", "Android","WebApp", "TweetDeck")
+# Table with intersecting sources
+sourceTable <- matrix(c(5,347,20,458,154,  7,306,11,264,195), nrow=2, byrow=TRUE)
+colnames(sourceTable) <- intersect(names(TSTable), names(RSTable))
 rownames(sourceTable) <- c("Tweets","Random")
 print(sourceTable)
 
@@ -55,3 +79,6 @@ print(sourceTable)
 # -- 8.4.17
 # -- 8.4.18
 # -- 8.4.19
+
+#Testing Commit
+x = 2
