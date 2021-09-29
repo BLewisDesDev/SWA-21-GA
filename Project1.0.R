f4d466a21323e863e023d37dc0424b62a6cb7f10
Byron <b.lewis.design.development@gmail.com>
2021-09-29 19:19:54 +1000

    Working 8.1

--- Project1.0.R
+++ Project1.0.R
@@ -1,10 +1,10 @@
-# -- -- -- SWA Group XXXX Major Project -- -- -- #
+# -- -- -- SWA Group X 2021 Major Project -- -- -- #
 
 # -- Libraries
 library(plyr)
 library(dplyr)
 library(readxl)
-library(mosaic) #not working for me?
+library(mosaic)
 library(ggplot2)
 
 # -- Directory (Add your working directory here)
@@ -30,17 +30,26 @@ count(random,source)
 RSTable = table(random$source)
 
 # -- 8.1.3 (Create table)
+AllSourceNames = c(intersect(names(TSTable), names(RSTable)),setdiff(names(TSTable), names(RSTable)),setdiff(names(RSTable), names(TSTable)))
+TweetSourceVect = c(5,347,20,458,154,1,1,1,1,1,2,1,1,1,1,5,replicate(36,0))
+TweetRandVect = c(7,306,11,264,195,replicate(11,0),4,1,2,1,2,1,1,1,15,1,1,2,2,1,3,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,2,1,1,149,5,4)
 
-# Table with intersecting sources (Not sure if we should include all the random sources or we should clean)
-
-sourceTable <- matrix(c(5,347,20,458,154,  7,306,11,264,195), nrow=2, byrow=TRUE)
-colnames(sourceTable) <- intersect(names(TSTable), names(RSTable))
+sourceTable <- matrix(c(TweetSourceVect,TweetRandVect), nrow=2, byrow=TRUE)
+colnames(sourceTable) <- AllSourceNames
 rownames(sourceTable) <- c("Tweets","Random")
 print(sourceTable)
 
+# -- 8.1.4 (Chi Squared Test for Independence)
+chisq <- chisq.test(sourceTable)
+chisq
 
-# -- 8.1.4
-# -- 8.1.5
+# -- 8.1.5 (Bootstrap Distribution for Iphone)
+sourceTable[7:8]
+colSums(sourceTable)
+rowSums(sourceTable)
+
+
+
 # -- 8.1.6
 
 # -- -- Question 8.2
